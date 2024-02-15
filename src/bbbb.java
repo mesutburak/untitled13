24911e96001a2bfba9ee9a82439d980fffbc90a3
mesutburak <boyka44444@gmail.com>
2024-02-15 13:18:18 +0300

    en son yaptıgım

--- src/bbbb.java
+++ src/bbbb.java
@@ -2,5 +2,6 @@ public class bbbb {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("hhh");
+        System.out.println("dddd");
     }
 }
